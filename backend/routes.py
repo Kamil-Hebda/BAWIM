029554af4c753c62826d2f8aba935dcf40c061e0
Kamil Hebda <kamilhebda28@gmail.com>
2024-11-17 14:58:11 +0100

    Update routes.py

--- backend/routes.py
+++ backend/routes.py
@@ -3,25 +3,36 @@ import sqlite3
 import time
 import psycopg2
 from models import db
+from flask import jsonify
+
 
 def init_routes(app):
     @app.route('/')
     def index():
         return render_template('index.html')
-
+    
     @app.route('/classic_sqli', methods=['GET', 'POST'])
     def classic_sqli():
-        if request.method == 'POST':
-            username = request.form['username']
-            password = request.form['password']
-            conn = sqlite3.connect('database.db')
+        return render_template('classic_sqli.html')
+
+    @app.route('/classic_sqli_login', methods=['GET', 'POST'])
+    def classic_sqli_login():
+        data=request.get_json()
+        if data:
+            username = data['username']
+            password = data['password']
+            conn = psycopg2.connect(app.config['SQLALCHEMY_DATABASE_URI'])
             cursor = conn.cursor()
+            # celowe wprowadzanie podatności sql injection
             query = f"SELECT * FROM users WHERE username = '{username}' AND password = '{password}'"
             cursor.execute(query)
-            result = cursor.fetchall()
+            result = cursor.fetchone()
             conn.close()
-            return render_template('classic_sqli.html', result=result)
-        return render_template('classic_sqli.html')
+            if result:
+                return jsonify({"status": "success", "message": "Login successfully", "result": result}), 200
+            else:
+                return jsonify({"status": "error", "message": "Invalid username or password"}), 400
+        return jsonify({"status": "error", "message": "No data provided"}), 400
 
     @app.route('/second_order_sqli', methods=['GET', 'POST'])
     def second_order_sqli():
